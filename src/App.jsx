2e09c33d1dfbb07c8eec3845930ae8378e539e38
Daniel Nobre <55868687+Dan-Nobre@users.noreply.github.com>
2026-01-12 11:23:34 -0300

    :test_tube: Testing UI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <p>Conteudo aqui nean</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+// src/components/Header.jsx
+import React from 'react';
+// import './Header.css';
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Oi, estou funcionando blz</h1>
+        </header>
+    );
+};
+
+export default Header;
