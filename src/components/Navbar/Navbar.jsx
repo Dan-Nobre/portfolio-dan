785ee0d2057331d1321f7f7157de5f018b74f027
Daniel Nobre <55868687+Dan-Nobre@users.noreply.github.com>
2026-01-15 14:44:22 -0300

    :lipstick: feat: testando estilos

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,6 @@
 import useLanguage from '/Users/danielnobre/Developer/Portfolio/portfolio-dan/src/hooks/useLanguage.js';
+import React, { useState, useEffect } from 'react';
+import './Navbar.css';
 
 const Navbar = () => {
   const { t, language, changeLanguage } = useLanguage();
@@ -9,41 +11,43 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        {/* Logo */}
-        <div className="navbar-logo">
-          <span>{t.navbar.logo}</span>
-        </div>
-        
-        {/* Navlinks att :p */}
-        <div className="navbar-links">
-          <button onClick={() => scrollToSection('about')}>
-            {t.navbar.about}
-          </button>
-          <button onClick={() => scrollToSection('projects')}>
-            {t.navbar.projects}
-          </button>
-          <button onClick={() => scrollToSection('footer')}>
-            {t.navbar.contact}
-          </button>
-        </div>
-        
-        <div className="language-switcher">
-          <button 
-            onClick={() => changeLanguage('pt')}
-            className={language === 'pt' ? 'active' : ''}
-          >
-            PT
-          </button>
-          <button 
-            onClick={() => changeLanguage('en')}
-            className={language === 'en' ? 'active' : ''}
-          >
-            EN
-          </button>
+      <div className="navbar-wrapper">
+        <div className="navbar-menu">
+          
+          <div className="navbar-logo">
+            <span>{t.navbar.logo}</span>
+          </div>
+
+          <div className="navbar-links">
+            <button onClick={() => scrollToSection('about')}>
+              {t.navbar.about}
+            </button>
+            <button onClick={() => scrollToSection('projects')}>
+              {t.navbar.projects}
+            </button>
+            <button onClick={() => scrollToSection('footer')}>
+              {t.navbar.contact}
+            </button>
+          </div>
+
+          <div className="language-switcher">
+            <button 
+              onClick={() => changeLanguage('pt')}
+              className={language === 'pt' ? 'active' : ''}
+            >
+              PT
+            </button>
+            <button 
+              onClick={() => changeLanguage('en')}
+              className={language === 'en' ? 'active' : ''}
+            >
+              EN
+            </button>
+          </div>
         </div>
       </div>
     </nav>
+
   );
 };
 
